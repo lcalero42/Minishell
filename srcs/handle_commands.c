0bbbaedd82b20200d422f4cd59b7ee35527c900b
Luis Calero <luis.calero1777@gmail.com>
2025-02-18 20:33:12 +0100

    fix: cd or pwd working when not typed entirely

--- srcs/handle_commands.c
+++ srcs/handle_commands.c
@@ -6,7 +6,7 @@
 /*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 14:12:49 by lcalero           #+#    #+#             */
-/*   Updated: 2025/02/18 19:31:07 by lcalero          ###   ########.fr       */
+/*   Updated: 2025/02/18 20:31:21 by lcalero          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	handle_commands(t_data *data)
 {
 	if (!data->cmd[0])
 		return ;
-	if (!ft_strncmp("pwd", data->cmd[0], ft_strlen(data->cmd[0])))
+	if (!ft_strncmp("pwd", data->cmd[0], ft_strlen(data->cmd[0]))
+			&& ft_strlen(data->cmd[0]) == 3)
 		pwd();
-	if (!ft_strncmp("cd", data->cmd[0], ft_strlen(data->cmd[0])))
+	if (!ft_strncmp("cd", data->cmd[0], ft_strlen(data->cmd[0]))
+			&& ft_strlen(data->cmd[0]) == 2)
 		cd(data->cmd[1]);
 }
