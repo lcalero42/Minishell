00a498dab38ecdbe8c4a37cc4e5b34b49e1a6fb5
Luis Calero <luis.calero1777@gmail.com>
2025-02-19 16:30:06 +0100

    feature: unknown command handling (error message)

--- srcs/handle_commands.c
+++ srcs/handle_commands.c
@@ -6,26 +6,37 @@
 /*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 14:12:49 by lcalero           #+#    #+#             */
-/*   Updated: 2025/02/19 16:08:28 by lcalero          ###   ########.fr       */
+/*   Updated: 2025/02/19 16:26:55 by lcalero          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+static void	handle_unknown_command(t_data *data);
+
 void	handle_commands(t_data *data, char **envp)
 {
 	if (!data->cmd[0])
 		return ;
-	if (!ft_strncmp("pwd", data->cmd[0], ft_strlen(data->cmd[0]))
+	else if (!ft_strncmp("pwd", data->cmd[0], ft_strlen(data->cmd[0]))
 		&& ft_strlen(data->cmd[0]) == 3)
 		pwd();
-	if (!ft_strncmp("cd", data->cmd[0], ft_strlen(data->cmd[0]))
+	else if (!ft_strncmp("cd", data->cmd[0], ft_strlen(data->cmd[0]))
 		&& ft_strlen(data->cmd[0]) == 2)
 		cd(data->cmd[1]);
-	if (!ft_strncmp("echo", data->cmd[0], ft_strlen(data->cmd[0]))
+	else if (!ft_strncmp("echo", data->cmd[0], ft_strlen(data->cmd[0]))
 		&& ft_strlen(data->cmd[0]) == 4)
 		echo(data);
-	if (!ft_strncmp("env", data->cmd[0], ft_strlen(data->cmd[0]))
+	else if (!ft_strncmp("env", data->cmd[0], ft_strlen(data->cmd[0]))
 		&& ft_strlen(data->cmd[0]) == 3)
 		env(envp);
+	else
+		handle_unknown_command(data);
+}
+
+static void	handle_unknown_command(t_data *data)
+{
+	ft_putstr_fd(data->cmd[0], 2);
+	ft_putstr_fd(": Command not found", 2);
+	ft_putchar_fd('\n', 2);
 }
