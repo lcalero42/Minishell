95c353fe9fa3c45c5927b876be8385f49bb06f92
Enzo Keisler <enzokeisler89@gmail.com>
2025-03-12 15:39:36 +0100

    Chore: remove comment

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,12 @@
 /*   By: ekeisler <ekeisler@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 15:25:58 by lcalero           #+#    #+#             */
-/*   Updated: 2025/03/12 15:38:56 by ekeisler         ###   ########.fr       */
+/*   Updated: 2025/03/12 15:39:20 by ekeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-// main
 int	main(int ac, char **av, char **envp)
 {
 	t_data	data;
