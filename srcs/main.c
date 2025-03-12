e8d1892c0b6f4dfd54583d95eec32d185d3e0cdf
Enzo Keisler <enzokeisler89@gmail.com>
2025-03-12 15:39:06 +0100

    Chore: normed

--- srcs/main.c
+++ srcs/main.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ekeisler <ekeisler@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 15:25:58 by lcalero           #+#    #+#             */
-/*   Updated: 2025/03/12 14:11:05 by lcalero          ###   ########.fr       */
+/*   Updated: 2025/03/12 15:38:56 by ekeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+// main
 int	main(int ac, char **av, char **envp)
 {
 	t_data	data;
