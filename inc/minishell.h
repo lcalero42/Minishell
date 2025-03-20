fd101721926682d49eb0106784694b6c4d1e3c67
Enzo Keisler <enzokeisler89@gmail.com>
2025-03-20 14:31:58 +0100

    style: Reorganized .h

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,13 +6,14 @@
 /*   By: ekeisler <ekeisler@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 15:26:07 by lcalero           #+#    #+#             */
-/*   Updated: 2025/03/18 17:13:32 by ekeisler         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:30:54 by ekeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+// INCLUDES                                    
 # include "../libft/libft.h"
 # include <stdio.h>
 # include <readline/readline.h>
@@ -21,26 +22,21 @@
 # include <signal.h>
 # include <limits.h>
 
+// DEFINES
 # define MAX_ARGS 64
 
+// ENUMERATIONS
 typedef enum e_token_type
 {
-	WORD,			//Command and args
-	PIPE,			// '|'
-	REDIR_IN,		// '<'
-	REDIR_OUT,		// '>'
-	REDIR_APPEND,	// '>>'
-	HEREDOC,		// '<<'
-	ENV_VAR,		// '$VAR'
-	QUOTE,			// '"' or '''
+	WORD,			// Command and arguments 
+	PIPE,			// '|' 
+	REDIR_IN,		// '<' 
+	REDIR_OUT,		// '>' 
+	REDIR_APPEND,	// '>>' 
+	HEREDOC,		// '<<' 
+	ENV_VAR,		// '$VAR' 
+	QUOTE,			// '"' or ''' 
 }	t_token_type;
-
-typedef struct s_token
-{
-	char			*value;
-	t_token_type	type;
-	struct s_token	*next;
-}	t_token;
 
 typedef enum e_redir_type
 {
@@ -48,8 +44,16 @@ typedef enum e_redir_type
 	REDIR_INPUT,
 	REDIR_OUTPUT,
 	REDIR_APPEND_OUT,
-	REDIR_HEREDOC
+	REDIR_HEREDOC,
 }	t_redir_type;
+
+// DATA STRUCTURE
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
 
 typedef struct s_redirection
 {
@@ -77,68 +81,50 @@ typedef struct s_data
 	int			exit_status;
 }	t_data;
 
+// TOKENIZER FUNCTION
 t_token		*tokenize(char *input, t_data *data);
-
 char		*extract_quoted_string(char *str);
-
 char		*extract_word(char *str);
-
 void		add_token(t_token **tokens, char *value, t_token_type type);
-
 int			ft_isspace(char c);
 
+// PARSER FUNCTION
 t_command	*parse_commands(t_token *token_list);
-
 t_command	*init_command(void);
-
 void		add_command(t_command **cmd_list, t_command *new_cmd);
-
 void		add_argument(t_command *cmd, char *arg);
-
 void		add_redirection(t_command *cmd, char *file, t_redir_type type);
 
+// MEMORY MANAGEMENT
 void		free_tokens(t_token *tokens);
-
 void		free_commands(t_command *cmd_list);
-
 void		free_redirections(t_redirection *redir);
-
-void		loop(t_data *data);
-
-int			make_env(t_data *data, char **envp);
-
 void		ft_free(char **cmd);
-
 void		ft_free_env(t_data *data);
 
-void		print_welcome(void);
-
-void		setup_signal(void);
-
-void		pwd(void);
-
-void		echo(t_data *data);
-
-void		env(char **env, t_data *data);
-
-void		unset(char *var, char **envp);
-
-void		handle_commands(t_data *data);
-
-void		cd(char *s, t_data *data);
-
-void		export(t_data *data);
-
-void		ft_exit(t_data *data);
-
+// ENVIRONMENT FUNCTIONS
+int			make_env(t_data *data, char **envp);
 char		*expand_variable(char *input, t_data *data);
-
 char		*ft_getenv(t_data *data, char *s);
 
-char		*ft_strncpy(char *destination, const char *source, size_t length);
+// BUILTIN COMMANDS
+void		pwd(void);
+void		echo(t_data *data);
+void		env(char **env, t_data *data);
+void		unset(char *var, char **envp);
+void		cd(char *s, t_data *data);
+void		export(t_data *data);
+void		ft_exit(t_data *data);
 
+// EXECUTION FUNCTIONS
+void		loop(t_data *data);
+void		handle_commands(t_data *data);
 void		exec_cmd(t_data *data);
-
 void		handle_unknown_command(char *cmd, t_data *data);
+
+// UTILITY FUNCTIONS
+void		print_welcome(void);
+void		setup_signal(void);
+char		*ft_strncpy(char *destination, const char *source, size_t length);
 
 #endif
