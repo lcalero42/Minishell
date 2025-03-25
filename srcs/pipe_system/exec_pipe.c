53c37ad1d8089fd3034ee5a9a4460c1b9f6f1150
Luis Calero <luis.calero1777@gmail.com>
2025-03-25 14:53:14 +0100

    feature: pipe system for builtin commands

--- srcs/pipe_system/exec_pipe.c
+++ srcs/pipe_system/exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 15:26:06 by lcalero           #+#    #+#             */
-/*   Updated: 2025/03/24 17:48:43 by lcalero          ###   ########.fr       */
+/*   Updated: 2025/03/25 14:51:33 by lcalero          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,59 @@ void	exec(t_data *data)
 	int			fd[2];
 	pid_t		pid;
 	int			fd_in;
+	int			status;
 
 	cmd = data->commands;
-	fd_in = 0;
+	fd_in = STDIN_FILENO;
 	while (cmd)
 	{
+		fd[0] = -1;
+		fd[1] = -1;
 		if (cmd->next)
-			pipe(fd);
+		{
+			if (pipe(fd) == -1)
+			{
+				perror("pipe");
+				return;
+			}
+		}
 		pid = fork();
 		if (pid == 0)
 		{
-			if (fd_in != 0)
+			if (fd_in != STDIN_FILENO)
 			{
-				dup2(fd_in, 0);
+				if (dup2(fd_in, STDIN_FILENO) == -1)
+				{
+					perror("dup2 input");
+					exit(1);
+				}
 				close(fd_in);
 			}
 			if (cmd->next)
 			{
-				dup2(fd[1], 1);
+				if (dup2(fd[1], STDOUT_FILENO) == -1)
+				{
+					perror("dup2 output");
+					exit(1);
+				}
+			}
+			if (cmd->next)
+			{
+				close(fd[0]);
 				close(fd[1]);
 			}
-			close(fd[0]);
 			apply_redirections(cmd);
 			find_cmd(cmd, data);
+			free_all(NULL, data, data->commands);
+			ft_free_env(data);
+			exit(0);
 		}
-		if (fd_in != 0)
+		else if (pid < 0)
+		{
+			perror("fork");
+			return;
+		}
+		if (fd_in != STDIN_FILENO)
 			close(fd_in);
 		if (cmd->next)
 		{
@@ -53,7 +81,7 @@ void	exec(t_data *data)
 		}
 		cmd = cmd->next;
 	}
-	while (wait(NULL) > 0);
+	while (wait(&status) > 0);
 }
 
 int	check_pipe(t_data *data)
@@ -109,4 +137,3 @@ static void	find_cmd(t_command *command, t_data *data)
 // 		return (1);
 // 	return (0);
 // }
-
